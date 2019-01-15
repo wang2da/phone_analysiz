fbd5b12aeca6844fac606681533da332757f35d3
wang2da <1563510144@qq.com>
2019-01-15 17:32:59 +0800

    添加set/get方法

--- src/test/java/com/phone/learning.java
+++ src/test/java/com/phone/learning.java
@@ -3,4 +3,11 @@ package com.phone;
 public class learning {
     String name;
     int age;
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
 }
